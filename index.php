c401d96831b7d20b2ff8c3f50c5e5d12b36c3a31
mr.dougladmorais23@gmail.com <mr.dougladmorais23@gmail.com>
2020-05-14 13:36:27 -0300

    php update 301 redirect

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://jobs.abstartups.com.br/", true, 301);
+exit();
+?>
---
+++ talentos/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://jobs.abstartups.com.br/talentos", true, 301);
+exit();
+?>
